7ba1f5417f11dbd7a9a1c5250b6f934ac865e279
WayJasy <30669860@qq.com>
2020-01-25 23:33:21 +0800

    init

---
+++ src/main/java/blog/blogdemo/BlogdemoApplication.java
@@ -0,0 +1,13 @@
+package blog.blogdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogdemoApplication.class, args);
+    }
+
+}
