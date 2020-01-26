c2f4eb281013b7ec2a5e4840e5368edf64bcdd2e
WayJasy <30669860@qq.com>
2020-01-26 20:02:14 +0800

    session

--- src/main/java/blog/blogdemo/Controller/AuthorizeController.java
+++ src/main/java/blog/blogdemo/Controller/AuthorizeController.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 @Controller
@@ -21,34 +22,30 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
-    @Value("$(github.client.id)") private String clientId;
-    @Value("$(github.client.secret)") private String clientSecret;
-    @Value("$(github.redirect.uri)") private String redirectUri;
+    @Value("${github.client.id}") private String clientId;
+    @Value("${github.client.secret}") private String clientSecret;
+    @Value("${github.redirect.uri}") private String redirectUri;
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
-                           @RequestParam(name="state") String state){
+                           @RequestParam(name="state") String state,
+                           HttpServletRequest request){
         Access_tokenDto access_tokenDto = new Access_tokenDto();
         access_tokenDto.setClient_id(clientId);
         access_tokenDto.setClient_secret(clientSecret);
         access_tokenDto.setCode(code);
         access_tokenDto.setRedirect_uri(redirectUri);
         access_tokenDto.setState(state);
-        githubProvider.getAccessToken(access_tokenDto);
-        return "Index";
+        String accessToken = githubProvider.getAccessToken(access_tokenDto);
+        System.out.println(accessToken);
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        System.out.println("GGg"+githubUser);
+        if(githubUser != null){
+            request.getSession().setAttribute("user",githubUser);
+            return "redirect:/"; //重定向
+        }else{
+            return "redirect:/";
+        }
     }
 
-    public GithubUser getUser(String accessToken){
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder()
-                .url("https://api.github.com/user?access_token="+accessToken)
-                .build();
-        try (Response response = client.newCall(request).execute()) {
-            String body = response.body().string();
-            GithubUser githubUser = JSON.parseObject(body, GithubUser.class); //将body转化为类对象
-            return githubUser;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+
 }
--- src/main/java/blog/blogdemo/Provider/GithubProvider.java
+++ src/main/java/blog/blogdemo/Provider/GithubProvider.java
@@ -1,6 +1,7 @@
 package blog.blogdemo.Provider;
 
 import blog.blogdemo.Dto.Access_tokenDto;
+import blog.blogdemo.Dto.GithubUser;
 import com.alibaba.fastjson.JSON;
 import okhttp3.*;
 import org.springframework.stereotype.Component;
@@ -24,6 +25,26 @@ public class GithubProvider {
             String string = response.body().string();
             return string;
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public GithubUser getUser(String accessToken){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?"+accessToken)
+                .build();
+        System.out.println(request);
+        try{
+            Response response = client.newCall(request).execute();
+            String body = response.body().string();
+            GithubUser githubUser = JSON.parseObject(body, GithubUser.class); //将body转化为类对象
+            System.out.println(body);
+            System.out.println(githubUser.getName());
+
+            return githubUser;
         } catch (IOException e) {
             e.printStackTrace();
         }
